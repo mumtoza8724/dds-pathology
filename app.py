fa66baea83625fd0dfef8c14a2e746423627308f
mumtoza8724 <taspulatovamumtoza@gmail.com>
2026-05-18 09:54:02 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -8,3 +8,7 @@ def home():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+from flask import send_from_directory
+@app.route('/google7836419afd22a9e3 (1).html')
+def google_verification():
+    return send_from_directory('', 'google7836419afd22a9e3 (1).html')
