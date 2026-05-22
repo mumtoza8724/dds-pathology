ea07dc68b5d22a5a8256c95c2d316ba11e48632d
mumtoza8724 <taspulatovamumtoza@gmail.com>
2026-05-22 14:46:06 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
-@app.route("/")
+@app.route('/')
 def home():
     return render_template("index.html")
 
 @app.route('/robots.txt')
 def robots():
-    return send_from_directory('static', 'robots.txt')
+    return send_from_directory(app.static_folder, 'robots.txt')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
